760b94b7f9ad78fefed9e1fed91237a8e7e01501
francisco <francisco20708@hotmail.com>
2019-12-08 15:03:36 -0600

    test_forms

--- usuario/tests/test_forms.py
+++ usuario/tests/test_forms.py
@@ -4,9 +4,6 @@ from trabajador.forms import TrabajadorForm
 
 class TestFormModificarUsuario(TestCase):
     
-    #43 name
-    
-
     def test_si_el_formulario_es_invalido_mas_de_43_caracteres_en_username(self):
 
         data = {
@@ -19,6 +16,48 @@ class TestFormModificarUsuario(TestCase):
         )
         self.assertFalse(form.is_valid())
         
-    def test_si_el_formulario_es_invalido_sin_nombre(self):
-        pass
+    def test_si_el_formulario_es_invalido_sin_username(self):
+        data = {
+            'username':'',
+            'email':'tigre@hotmail.com',
+        }
+        
+        form = TrabajadorForm(
+            data
+        )
+        self.assertFalse(form.is_valid())
+        
+    def test_si_el_email_es_incorrecto_incompleto(self):
+        data = {
+            'username':'tigrito',
+            'email':'tigre@',
+        }
+        
+        form = TrabajadorForm(
+            data
+        )
+        self.assertFalse(form.is_valid()) 
+        
+        
+    def test_si_el_email_es_incorrecto(self):
+        data = {
+            'username':'tigrito',
+            'email':'tigre@hot',
+        }
+        
+        form = TrabajadorForm(
+            data
+        )
+        self.assertFalse(form.is_valid()) 
+        
     
+    def test_si_el_formulario_es_invalido_sin_email(self):
+        data = {
+            'username':'tigrito',
+            'email':'',
+        }
+        
+        form = TrabajadorForm(
+            data
+        )
+        self.assertFalse(form.is_valid())
